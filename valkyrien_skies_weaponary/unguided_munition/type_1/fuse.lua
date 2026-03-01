003d9cc1e22fe2cdddb0ceed8b696d5cc50664ba
HydratedIronOxide <102597088+HydratedIronOxide@users.noreply.github.com>
2026-03-01 15:20:28 +0000

    Update fuse.lua

--- valkyrien_skies_weaponary/unguided_munition/type_1/fuse.lua
+++ valkyrien_skies_weaponary/unguided_munition/type_1/fuse.lua
@@ -5,8 +5,8 @@ local tick = 0
 while true do
     vel = ship.getVelocity()
 
-    if vel.length() > 3 then tick = tick + 1 end
-    if tick == 10 then arm = true end
+    if vel:length() > 3 then tick = tick + 1 end
+    if tick == 8 then arm = true end
 
     if arm and redstone.getAnalogInput('front') >= 13 then
         redstone.setOutput('back', true)
