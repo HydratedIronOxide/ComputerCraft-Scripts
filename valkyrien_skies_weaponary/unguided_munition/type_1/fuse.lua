9b1b89f1350d004bf49e840b59eeefd474d8acde
HydratedIronOxide <102597088+HydratedIronOxide@users.noreply.github.com>
2026-03-01 15:16:48 +0000

    Add fuse logic for unguided munition type 1

---
+++ valkyrien_skies_weaponary/unguided_munition/type_1/fuse.lua
@@ -0,0 +1,16 @@
+local vel = vector.new()
+local arm = false
+local tick = 0
+
+while true do
+    vel = ship.getVelocity()
+
+    if vel.length() > 3 then tick = tick + 1 end
+    if tick == 10 then arm = true end
+
+    if arm and redstone.getAnalogInput('front') >= 13 then
+        redstone.setOutput('back', true)
+    end
+
+    sleep(0.05)
+end
